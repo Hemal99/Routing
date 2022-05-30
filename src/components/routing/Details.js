428b35a6642336583b8641611595a51de57aa4ac
Hemal99 <h.githmin@gmail.com>
2022-05-30 10:39:42 +0530

    done

--- src/components/routing/Details.js
+++ src/components/routing/Details.js
@@ -1,12 +1,24 @@
 import React from "react";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
-import { Grid, TextField } from "@mui/material";
+import { Grid, TextField, Card, CardContent, Typography } from "@mui/material";
 
 export default function Header() {
   return (
-    <Grid container justifyContent={"center"} alignContent="center" p={2}  style={{ height:"100%",borderColor:"black"}} >
-      Details
-     
-    </Grid>
+    <Card m={1}>
+      <CardContent>
+        <Grid
+          container
+          justifyContent={"flex-start"}
+          alignContent="center"
+          p={2}
+          style={{ width: "100%", borderColor: "black" }}
+        >
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h6">Selected Dates</Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}></Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
